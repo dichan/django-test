4e1e948de52789f5941d63190c2d5d91e1e33f24
Diane <yangx431@gmail.com>
2014-03-19 22:38:06 -0500

    Modifies URL and view

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hello world!")
+	return HttpResponse("Rango says hello world! <a href='/rango/about'>About Page</a>")
+
+def about(request):
+	return HttpResponse("Rango Says: Here is the about page. <a href='..'>Home Page</a>")
